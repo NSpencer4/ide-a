3d2ec8fa2d8fcc693d18fc9a14ba47b63a4f6f3a
NSpencer4 <n.chase.spencer@gmail.com>
2026-02-14 22:03:43 -0500

    update RSC entry point and disable observability in wrangler config

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,6 @@
 import { Welcome } from "../welcome/welcome";
 
-export default async function Home() {
+export default function Home() {
     const message = "Hello from RSC on Cloudflare Workers";
 
     return <Welcome message={message}/>;
